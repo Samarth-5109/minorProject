29b9e8e85795a7371b1eee746a37176f8737b5ce
Samarth Bhardwaj <85241456+Samarth-5109@users.noreply.github.com>
2021-12-04 20:13:46 +0530

    backend overhaul

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
 const express = require("express");
-//const fs = require('fs');
+const fs = require('fs');
 const ytdl = require('ytdl-core');
 const app = express();
-var cors = require('cors')
-
-
+var cors =require('cors')
 
 app.use(cors())
 
+
+
 // app.get("/",(req,res)=>{
-//     ytdl('https://youtu.be/W1CzJ4bvD4M')
-//   .pipe(fs.createWriteStream('video.mp4'));
-//     res.send("hello")
-// })
+//          ytdl('https://youtube.com/watch?v=lLiw3egVAZU&t=2s')
+//       .pipe(fs.createWriteStream('video.mp4'));
+//          res.send("hello")
+//  })
 
-// functions
-
-const convertUrl = (url)=>{
+const convertUrl = (url) =>{
 
     let newUrlArray;
     if(url.includes("youtu.be")){
-         newUrlArray = url.split("https://youtu.be/")
-         return `https://youtube.com/watch?v=${newUrlArray[1]}`
-    }else
-    if(url.includes("https://youtube.com/shorts/")){
 
-        newUrlArray = url.split("https://youtube.com/shorts/")
+         newUrlArray = url.split("https://youtu.be/")
         return `https://youtube.com/watch?v=${newUrlArray[1]}`
-   }
-   else if(url.includes("https://youtube.com/watch?v")){
-       return url
-   }
-   else{
-       return "https://youtube.com/watch?v=dQw4w9WgXcQ"
-   }
+    }
+    else if(url.includes("https://youtube.com/shorts/")){
+
+         newUrlArray = url.split("https://youtube.com/shorts/")
+        return `https://youtube.com/watch?v=${newUrlArray[1]}`
+    }
+    else if(url.includes("https://youtube.com/watch?v")){
+        return url
+    }
+    else{
+        return "https://youtube.com/watch?v=dQw4w9WgXcQ"
+    }
 }
 
 //video
+
 app.get("/video",async(req,res)=>{
-    console.log(req.query.videoId)
-    const videoId = convertUrl(req.query.videoId)
-     let info = await ytdl.getInfo(videoId)
-     res.json(info)
-})
+        const videoId = convertUrl(req.query.videoId)
+         let info = await ytdl.getInfo(videoId)
+         res.json(info)
+    })
 //audio
-app.get("/audio",async(req,res)=>{
-    const videoId = convertUrl(req.query.videoId)
-    let info = await ytdl.getInfo(videoId)
-    let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    res.json(audioFormats)
-})
+    app.get("/audio",async(req,res)=>{
+        const videoId = convertUrl(req.query.videoId)
+         let info = await ytdl.getInfo(videoId)
+         let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+         res.json(audioFormats)
+    })
 
-//app.listen(4000,console.log("working..."))
+// download video 
+    app.get("/download",async(req,res)=>{
 
-//download video download
-app.get("/download",async(req,res)=>{
-    const {itag,title,type} = req.query
-    const videoId = convertUrl(req.query.videoId)
-    if(type === "mp4"){
+        const {itag, title,type} = req.query
+        const videoId = convertUrl(req.query.videoId)
+
+       if(type === "mp4"){
 		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
 		res.header('Content-Type', 'video/mp4');
 	}else if(type === "mp3"){
@@ -66,8 +65,7 @@ app.get("/download",async(req,res)=>{
 	}
 
 	ytdl(videoId, { filter: format => format.itag === parseInt(itag) }).pipe(res); 
-	
+    })
 
-    
-})
-app.listen(4000,console.log("working..."))
+
+app.listen(4000,console.log("working...")) 
